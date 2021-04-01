da16c3fcb099e3bfafd8dc03bed97f4dd934ffcd
Marek Turadek <m.turadek@f5.com>
2021-04-01 21:57:11 +0200

    Delete images from legacy-s3

--- update_rds.py
+++ update_rds.py
@@ -18,8 +18,10 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
 
+OLD_PATH = 'image/'
+NEW_PATH = 'avatar/'
 
-# TODO: SELECT from database all rows with image/ avatars
+
 def select_legacy_images_from_db(connection, path):
     cursor = None
     rows = []
@@ -38,7 +40,6 @@ def select_legacy_images_from_db(connection, path):
         return rows
 
 
-# TODO: Copy all images to production-s3
 def copy_objects_between_buckets(source_bucket, destination_bucket, source_list, new_path):
     try:
         s3 = boto3.resource('s3', region_name=AWS_DEFAULT_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -54,7 +55,6 @@ def copy_objects_between_buckets(source_bucket, destination_bucket, source_list,
         sys.exit(1)
 
 
-# TODO: Update DB with links from production-s3
 def update_image_path_in_database(connection, source_list, new_path):
     cursor = None
     query = "UPDATE avatars SET path = %s WHERE id = %s"
@@ -68,27 +68,47 @@ def update_image_path_in_database(connection, source_list, new_path):
             cursor = connection.cursor()
             cursor.execute(query, fields)
             connection.commit()
+
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(f"Error inserting to the database: {error}")
         sys.exit(1)
     finally:
-        cursor.close()
+        if cursor:
+            cursor.close()
 
 
-# TODO: check DB against image/ avatars and if not, remove images from legacy-s3
+def delete_images_from_legacy_bucket(source_list):
+    check = select_legacy_images_from_db(conn, OLD_PATH)
+
+    if check:
+        print("There are still images with old path in DB")
+        sys.exit(2)
+    else:
+        print("Remove files from legacy-s3")
+        try:
+            s3 = boto3.resource('s3', region_name=AWS_DEFAULT_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+            for obj in source_list:
+                s3.Object(S3_LEGACY_BUCKET_NAME, obj[1]).delete()
+
+        except Exception as error:
+            logging.error(f"Error copying between S3 buckets: {error}")
+            sys.exit(1)
+
 
 if __name__ == '__main__':
 
     # Connect to db
     try:
         conn = psycopg2.connect(DB_CONN_STRING)
-        # conn.autocommit = True
     except Exception as e:
         logging.error(f"Error while connecting to the database: {e}")
         sys.exit(1)
 
-    data = select_legacy_images_from_db(conn, 'image/')
-    copy_objects_between_buckets(S3_LEGACY_BUCKET_NAME, S3_PRODUCTION_BUCKET_NAME, data, 'avatar/')
-    update_image_path_in_database(conn, data, 'image/')
+    data = select_legacy_images_from_db(conn, OLD_PATH)
+    copy_objects_between_buckets(S3_LEGACY_BUCKET_NAME, S3_PRODUCTION_BUCKET_NAME, data, NEW_PATH)
+    update_image_path_in_database(conn, data, NEW_PATH)
+    delete_images_from_legacy_bucket(data)
 
     conn.close()
